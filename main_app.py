aa24a332c79e88c9f93246b44775e66fd74f4f2e
Rigava <josharun1@gmail.com>
2024-05-22 13:20:25 +0530

    first commit

---
+++ main_app.py
@@ -0,0 +1,86 @@
+# https://www.youtube.com/watch?v=MoqgmWV1fm8
+
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+import os
+from langchain.embeddings import GooglePalmEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+# from langchain.chains import ConversationalRetrievalChain
+from langchain.llms import GooglePalm
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredURLLoader
+import pickle
+import time
+from pathlib import Path
+
+def init():
+    # Load the OpenAI API key from the environment variable
+    load_dotenv()
+    
+    # test that the API key exists
+    if os.getenv("GOOGLE_API_KEY") is None or os.getenv("GOOGLE_API_KEY") == "":
+        print("API_TOKEN is not set")
+        exit(1)
+    else:
+        print("API_TOKEN is set")
+    st.set_page_config(
+        page_title="Chat with your PDFs",
+        page_icon=":books"
+    )
+init()
+
+st.title("Akj News Research Tool 📈")
+st.sidebar.title("News Article URLs")
+
+urls = []
+for i in range(3):
+    url = st.sidebar.text_input(f"URL {i+1}")
+    urls.append(url)
+
+process_url_clicked = st.sidebar.button("Process URLs")
+
+main_placeholder = st.empty()
+GOOGLE_API_KEY = "AIzaSyAKEaaM7fWIErN3VbikjP_T5m0UfhBy5iE"
+llm = GooglePalm(google_api_key=GOOGLE_API_KEY)
+
+query = main_placeholder.text_input("Question: ") 
+
+if process_url_clicked:
+   
+    # load data
+    loader = UnstructuredURLLoader(urls=urls)
+    main_placeholder.text("Data Loading...Started...✅✅✅")
+    data = loader.load()
+    # split data
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=['\n\n', '\n', '.', ','],
+        chunk_size=1000
+    )
+    main_placeholder.text("Text Splitter...Started...✅✅✅")
+    docs = text_splitter.split_documents(data)
+    # create embeddings and save it to FAISS index
+    embeddings = GooglePalmEmbeddings(google_api_key=GOOGLE_API_KEY)
+    vectorstore_palm = FAISS.from_documents(docs, embeddings)
+    main_placeholder.text("Embedding Vector Started Building...✅✅✅")
+    time.sleep(2)
+
+    # Save the vectorstore object locally
+    vectorstore_palm.save_local("vectorstore")   
+    # Load the vectorstore object
+    x = FAISS.load_local("vectorstore", embeddings,allow_dangerous_deserialization=True)
+
+    chain = RetrievalQA.from_chain_type(llm =llm,
+        chain_type="stuff",
+        retriever=x.as_retriever(),
+        input_key ="query",
+        return_source_documents=True)
+    # res = chain("What is the pooling concept?")
+    res = chain(query)
+
+    # result will be a dictionary of this format --> {"answer": "", "sources": [] }
+    st.header("Answer")
+    st.write(res['result'])
+    st.write(res['source_documents'])
+
