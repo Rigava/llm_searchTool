c1b0b422ea28da701048011bc9cd901e398d8c89
NetZero <73232007+Rigava@users.noreply.github.com>
2025-02-15 15:59:18 +0530

    Update smartBlogger.py
    groq

--- smartBlogger.py
+++ smartBlogger.py
@@ -8,7 +8,8 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain.chains.question_answering import load_qa_chain
-from langchain_community.llms import GooglePalm
+# from langchain_community.llms import GooglePalm
+from langchain_groq import ChatGroq
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import WebBaseLoader
 import google.generativeai as palm
